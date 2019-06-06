c624a4612d8b8788520dd5e979ee4aa05bab350c
studentvmautocad <fcausevic1@etf.unsa.ba>
2019-06-06 09:43:44 +0200

    Initial commit

---
+++ src/ba/unsa/etf/rs/BusController.java
@@ -0,0 +1,47 @@
+package ba.unsa.etf.rs;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class BusController {
+    public TextField fldMaker;
+    public TextField fldSerija;
+    public TextField fldBrSjedista;
+    public Spinner spinerBrVozaca;
+    private Bus bus;
+    public BusController(Bus bus) {
+        this.bus = bus;
+    }
+
+    @FXML
+    public void initialize() {
+        if (bus != null) {
+            fldMaker.setText(bus.getProizvodjac());
+            fldSerija.setText(bus.getSerija());
+            fldBrSjedista.setText(String.valueOf(bus.getNumberOfSeats()));
+            spinerBrVozaca.setPromptText(String.valueOf(bus.getNumberOfDrivers()));
+        }
+    }
+
+    public void akcijaCancel(ActionEvent actionEvent) {
+        bus = null;
+        Stage stage = (Stage) fldMaker.getScene().getWindow();
+        stage.close();
+    }
+
+    public void akcijaOk(ActionEvent actionEvent) {
+        if (bus == null) bus = new Bus();
+        bus.setProizvodjac(fldMaker.getText());
+        bus.setSerija(fldSerija.getText());
+        Stage stage = (Stage) fldMaker.getScene().getWindow();
+        stage.close();
+    }
+
+
+
+    public Bus getBus() {
+        return bus;
+    }
+}
